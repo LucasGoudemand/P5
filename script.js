233cc2775f6f80e681959c33197cc23b1f152446
Lucas Goudemand <106696971+LucasGoudemand@users.noreply.github.com>
2022-12-09 15:07:42 +0100

    JS page accueil OK
    Affichage des produits sur la page Index.html via l'API ok

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
-fetch('http://localhost:3000/api/products')
+
+fetch("http://localhost:3000/api/products")
   .then(function(res) {
     if (res.ok) {
       return res.json();
     }
   })
-  .then(function(value) {
-    console.log(value.objects.colors);
-  document.getElementById("hello-result").innerText = value.name ;
-  
+  //création de la fonction lProduct qui est le resultat du FETCH
+  .then(function(lProduct) {
+    //tests
+  console.log(lProduct[0].imageUrl);
+
+  //selection de l'id ITEMS pour l'ajout des canapés et création de la variable element
+  const element = document.getElementById('items');
+  //création de la balise <a>
+  for (let i = 0; i < lProduct.length; i++) {
+
+  let newLink = document.createElement("a")
+  element.appendChild(newLink);
+  newLink.setAttribute('href', './product.html?id=' + lProduct[i]._id );
+
+  //création de la balise <article>
+  let newArticle = document.createElement("article")
+  newLink.appendChild(newArticle);
+
+  //création de la balise <img>
+  let newImg = document.createElement("img")
+  newArticle.appendChild(newImg);
+  newImg.setAttribute('src', lProduct[i].imageUrl);
+  newImg.setAttribute('alt', lProduct[i].altTxt);
+
+  //création de la balise <h3>
+  let newH3 = document.createElement("h3")
+  newArticle.appendChild(newH3);
+  newH3.classList.add("productName");
+
+  //création de la balise <p>
+  let newP = document.createElement("p")
+  newArticle.appendChild(newP);
+  newP.classList.add("productDescription");
+
+ 
+  //Ajout des lignes à la page INDEX
+
+  //Ajout de l'image du canapé
+  //newImg.innerHTML += lProduct[0].imageUrl ;
+  //Ajout du titre de l'article
+  newH3.innerHTML += lProduct[i].name;
+  //Ajout de la description de l'article
+  newP.innerHTML += lProduct[i].description;
+
+  }
+
   })
   .catch(function(err) {
     // Une erreur est survenue
   });
+
+ 
